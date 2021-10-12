124637a28c13972438ae1545532e48998dd5dbf9
Stanislav Akhmadeev <stanislav_akhmadeev@epam.com>
2021-10-12 20:18:40 +0400

    refactor: task 1

--- 04_js_core/app.js
+++ 04_js_core/app.js
@@ -7,40 +7,31 @@ function fn1() {
     }
 
     var regForStringWithoutExtra = /\s+|[а-яА-ЯЁёa-zA-Z\[\]()!&?`,#$@^_%<>=]+(=\s*\d*)/g;
-
     var stringNew = stringIn.replace(regForStringWithoutExtra, '');
+
     var arrayNumber = stringNew.match(/(\d+)(\.)?(\d*)?/g);
     var arrayMathSymbols = stringNew.match(/\+|-|\/|\*/g);
+    var result = 0;
+    var startIndexOfNumber = 0;
 
-    if (/\+|-|\/|\*/.test(stringNew[0])) {
-        var result = 0;
-        for (var i = 0, k = 0; k < arrayMathSymbols.length; i++, k++) {
-            switch (arrayMathSymbols[k]) {
-                case '*': result *= parseFloat(arrayNumber[i]);
-                    break;
-                case '/': result /= parseFloat(arrayNumber[i]);
-                    break;
-                case '+': result += parseFloat(arrayNumber[i]);
-                    break;
-                case '-': result -= parseFloat(arrayNumber[i]);
-                    break;
-            }
-        }
-    } else {
-        var result = +arrayNumber[0];
-        for (var i = 1, k = 0; k < arrayMathSymbols.length; i++, k++) {
-            switch (arrayMathSymbols[k]) {
-                case '*': result *= parseFloat(arrayNumber[i]);
-                    break;
-                case '/': result /= parseFloat(arrayNumber[i]);
-                    break;
-                case '+': result += parseFloat(arrayNumber[i]);
-                    break;
-                case '-': result -= parseFloat(arrayNumber[i]);
-                    break;
-            }
+    if (!(/\+|-|\/|\*/.test(stringNew[0]))) {
+        result = +arrayNumber[0];
+        startIndexOfNumber = 1;
+    }
+
+    for (var i = startIndexOfNumber, k = 0; k < arrayMathSymbols.length; i++, k++) {
+        switch (arrayMathSymbols[k]) {
+            case '*': result *= parseFloat(arrayNumber[i]);
+                break;
+            case '/': result /= parseFloat(arrayNumber[i]);
+                break;
+            case '+': result += parseFloat(arrayNumber[i]);
+                break;
+            case '-': result -= parseFloat(arrayNumber[i]);
+                break;
         }
     }
+
     stringOut.innerHTML = result.toFixed(2);
 }
 document.querySelector('.task-01-btn').onclick = fn1;
