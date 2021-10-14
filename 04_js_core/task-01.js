1401ad5a00267f7a8dc1d54889a08a4519d7e66b
Stanislav Akhmadeev <stanislav_akhmadeev@epam.com>
2021-10-14 16:26:37 +0400

    fix: task 1 add unary

--- 04_js_core/task-01.js
+++ 04_js_core/task-01.js
@@ -4,21 +4,23 @@ function solutionTaskOne() {
     var stringIn = document.querySelector('.task-01-in').value;
     var stringOut = document.querySelector('.task-01-out');
     var stringInValidEqualsContains = stringIn.indexOf('=') === -1;
-    var stringInValidEqualsOne = stringIn.match(/=/g).length > 1;
-    var stringInValid = /((-|\+|\*|\/)(\s*[а-яА-ЯЁёa-zA-Z\[\]()!&?`,#$@^_%<>]+)|((-|\+|\*|\/){2,})|(\d+[-|\+|\*|\/]\W+))/g;
+    var regStringInValid = /((-|\+|\*|\/)(\s*[а-яА-ЯЁёa-zA-Z\[\]()!&?`,#$@^_%<>]+)|((-|\+|\*|\/){3,})|(\d+[-|\+|\*|\/]+(\s)*=)|(\/{2})|(\*{2})|^(\+|-|\/|\*){2,})/g;
     var regForStringWithoutExtra = /\s+|[а-яА-ЯЁёa-zA-Z\[\]()!&?`,#$@^_%<>=]+(=\s*\d*)/g;
+    var regForArrayNumber = /(((?<=\+|-|\*|\\)(\+|-))([0-9]))|(\d+)(\.)?(\d*)?/g;
+    var regSymbols = /\+|-|\/|\*/g;
 
-    if (stringInValidEqualsContains || stringInValidEqualsOne || stringInValid.test(stringIn)) {
+    if (stringInValidEqualsContains || stringIn.match(/=/g).length > 1 || regStringInValid.test(stringIn)) {
         return stringOut.innerHTML = 'valid error';
     }
 
-    var stringNew = stringIn.replace(regForStringWithoutExtra, '');
-    var arrayNumber = stringNew.match(/(\d+)(\.)?(\d*)?/g);
-    var arrayMathSymbols = stringNew.match(/\+|-|\/|\*/g);
+    var stringWithoutExtra = stringIn.replace(regForStringWithoutExtra, '');
+    var arrayNumber = stringWithoutExtra.match(regForArrayNumber);
+    var stringWithoutExtraAndWithOutUnary = stringWithoutExtra.replace(regForArrayNumber, '');
+    var arrayMathSymbols = stringWithoutExtraAndWithOutUnary.match(regSymbols);
     var result = 0;
     var startIndexOfNumber = 0;
 
-    if (!(/\+|-|\/|\*/.test(stringNew[0]))) {
+    if (!(regSymbols.test(stringWithoutExtra[0]))) {
         result = +arrayNumber[0];
         startIndexOfNumber = 1;
     }
