bea7e7f80f60955058487b1677ddf2bddd366999
Stanislav <akhmadeevst@gmail.com>
2021-10-21 21:23:26 +0400

    refactor: SpaceShip add move

--- 05_js_game/assets/js/SpaceShip.js
+++ 05_js_game/assets/js/SpaceShip.js
@@ -1,10 +1,9 @@
-export function SpaceShip() {
-  // this.direction = 0;
+export const SpaceShip = function() {
   this.xPos = 10;
   this.yPos = 300;
   this.step = 4;
 
-  this.update = function (key) {
+  this.move = (key) => {
     if (key.has('ArrowUp')) {
       this.yPos -= this.yPos < 10 ? 0 : this.step;
     } else if (key.has('ArrowDown')) {
@@ -14,5 +13,9 @@ export function SpaceShip() {
     } else if (key.has('ArrowRight')) {
       this.xPos += this.xPos > 910 ? 0 : this.step;
     }
+  }
+
+  this.update = function (key) {
+    this.move(key);
   }
 }
