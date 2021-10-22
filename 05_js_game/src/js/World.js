cec97b166ea70a9cec1055d7b2dbf4833b108c40
Stanislav <akhmadeevst@gmail.com>
2021-10-22 17:37:06 +0400

    del: comment

--- 05_js_game/src/js/World.js
+++ 05_js_game/src/js/World.js
@@ -1,18 +1,8 @@
 import { randoms } from './utils/random.js'
 
 const World = function (SpaceShip, Asteroid) {
-  this.asteroidsPos = null;
   this.spaceShip = new SpaceShip();
   this.asteroid = new Asteroid();
-
-  this.setAsteroids = function (data) {
-    this.asteroidsPos = data.map(() => {
-      return {
-        x: 1024,
-        y: randoms(-20, 550),
-      }
-    });
-  }
 
   this.update = function (keys) {
     this.spaceShip.update(keys);
