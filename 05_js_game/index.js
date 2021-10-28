489e344872164da1e521849da30d0106598cd8a3
Stanislav <akhmadeevst@gmail.com>
2021-10-28 17:38:08 +0400

    refactor: add class hidden

--- 05_js_game/index.js
+++ 05_js_game/index.js
@@ -9,6 +9,8 @@ const btnsServices = document.querySelector('.btns-services');
 const btnGameNext = document.querySelector('.btn-service-next');
 const btnGamePause = document.querySelector('.btn-service-pause');
 const settings = {};
+const cvs = document.querySelector('canvas');
+const ctx = cvs.getContext('2d');
 
 export const gamePause = () => {
   if (!game.isPause) {
@@ -22,6 +24,10 @@ const gameLoop = () => {
   if (game.spaceShip.state && !game.isPause) {
     game.update(settings);
     game.render();
+  } else if (!game.spaceShip.state) {
+    ctx.fillStyle = "#ffffff";
+    ctx.font = "30px Verdana";
+    ctx.fillText(`Game Over ${game.score >= 0 ? game.score : 0}`, 430, 300);
   }
   window.requestAnimationFrame(() => {
     gameLoop();
@@ -31,28 +37,28 @@ const gameLoop = () => {
 body.addEventListener('click', (event) => {
   const btnsEvent = event.target;
   if (btnsEvent.classList.contains('btn-main-start')) {
-    btnMainStart.style.display = 'none';
-    ModalRegistration.style.display = 'block';
+    btnMainStart.classList.add('hidden');
+    ModalRegistration.classList.remove('hidden');
   } else if (btnsEvent.classList.contains('form-btn-start')) {
     event.preventDefault();
     settings.name = document.querySelector('.modal-registration-name').value === ''
       ? 'Unknown'
       : document.querySelector('.modal-registration-name').value;
     settings.difficult = document.querySelector('.modal-registration-difficult').value;
-    ModalRegistration.style.display = 'none';
-    btnsServices.style.display = 'flex';
+    ModalRegistration.classList.add('hidden');
+    btnsServices.classList.remove('hidden');
     gameLoop();
     renderAudios.createAudio(AUDIOS.begin);
   } else if (btnsEvent.classList.contains('form-btn-cancel')) {
-    btnMainStart.style.display = 'block';
-    ModalRegistration.style.display = 'none';
+    btnMainStart.classList.remove('hidden');
+    ModalRegistration.classList.add('hidden');
   } else if (btnsEvent.classList.contains('btn-service-next')) {
-    btnGameNext.style.display = 'none';
-    btnGamePause.style.display = 'inline-block';
+    btnGameNext.classList.add('hidden');
+    btnGamePause.classList.remove('hidden');
     gamePause();
   } else if (btnsEvent.classList.contains('btn-service-pause')) {
-    btnGameNext.style.display = 'inline-block';
-    btnGamePause.style.display = 'none';
+    btnGameNext.classList.remove('hidden');
+    btnGamePause.classList.add('hidden');
     gamePause();
   } else if (btnsEvent.classList.contains('btn-service-game')) {
     window.location.reload();
