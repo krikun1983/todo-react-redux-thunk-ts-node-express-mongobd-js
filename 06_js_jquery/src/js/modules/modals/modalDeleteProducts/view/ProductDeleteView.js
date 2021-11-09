058c85da262d55d62ca39f113e4c25a3fad4784a
Stanislav <akhmadeevst@gmail.com>
2021-11-09 20:06:15 +0400

    feat: add style modal delete

--- 06_js_jquery/src/js/modules/modals/modalDeleteProducts/view/ProductDeleteView.js
+++ 06_js_jquery/src/js/modules/modals/modalDeleteProducts/view/ProductDeleteView.js
@@ -8,11 +8,11 @@ class ProductDeleteView {
       `
         <div class="modal-delete-container">
           <div class="modal-delete">
-            <div class="modal-delete_product">Delete product ${productCurrent.name}?</div>
+            <div class="modal-delete_product">Delete product <span>${productCurrent.name}</span>?</div>
             <div class="modal-delete_question">Are you sure you want to perform this action</div>
             <div class="modal-delete_btns">
-              <button type="button" data-action="delete-yes">Yes</button>
-              <button type="button" data-action="delete-no" >No</button>
+              <button class="btn btn-primary" type="button" data-action="delete-yes">Yes</button>
+              <button class="btn btn-secondary" type="button" data-action="delete-no" >No</button>
             </div>
           </div>
         </div>
