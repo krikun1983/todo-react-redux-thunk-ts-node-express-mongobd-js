48afc67a55021a4751cb74b6b6777a38be3742d6
Stanislav <akhmadeevst@gmail.com>
2021-11-24 23:40:08 +0400

    fix: ui-button style path svg

--- 07_react_intro/07_react_intro/src/UI/UIButton/index.tsx
+++ 07_react_intro/07_react_intro/src/UI/UIButton/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import UiButtonProps from './type/ui-button-props';
 import cn from 'classnames';
-import style from './UIButton.module.scss';
+import style from './style/UIButton.module.scss';
 
 const UIButton: React.FC<UiButtonProps> = ({text, type, onClick, variant}) => {
   return (
